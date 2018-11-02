97a46d2b548c17b15a72d760cc83a2de9d57fc30
Kevin Cahill <kevin@Kevins-MacBook-Pro.local>
2018-11-01 23:52:34 -0600

    new soliton.m

--- soliton.m
+++ soliton.m
@@ -6,11 +6,11 @@ om1 = 32; om2 = 30;
 x = -10:.01:10;
 plot(x,tanh(x),'k-','linewidth',1.5)
 %axis([0.8  2.9  0  6])
-grid 'on'
+%grid 'on'
 axis([-10 10 -1.5 1.5])
 textx='$x$'
 xlabel(textx,'Interpreter','latex','fontsize',sf)
 texty=('$\phi(x) = \tanh(x)$')
 ylabel(texty,'Interpreter','latex','fontsize',sf)
 print -dpdf soliton
-print -deps soliton
+print -deps /Users/kevin/papers/math/soliton
